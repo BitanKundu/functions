7583cccc5aeda447d660def553c4e73ac3a7371a
BitanKundu <79818809+BitanKundu@users.noreply.github.com>
2021-10-30 07:01:02 -0700

    Add files via upload

---
+++ class98.py
@@ -0,0 +1,48 @@
+#functions are the set of instructions for doing some task or some work
+#function definition
+def add():
+    a=3
+    b=8
+    print(a+b)
+#functioncall
+add(
+)
+def subtract():
+    a=9
+    b=10
+    print(a-b)
+def multiplication():
+    a=9
+    b=3
+    print(a*b)
+multiplication()    
+def division():
+    a=7
+    b=6
+    print(a/b)
+division(e)
+def subtract():
+    a=5
+    b=9
+    print(a-b)
+
+
+
+def addition ():
+    a = 5
+    b = 6
+    print(a+b)
+
+def subtraction ():
+    a= 9
+    b= 7
+    print(a-b)
+
+def multplication():
+    a=5
+    b=9
+    print(a*b)
+def division():
+    a=6
+    b=8
+    print(a/b)
